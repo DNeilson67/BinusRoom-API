b6885429cfbdfaa9ca953155712cf34141a9db4d
Davin neilson <davin.neilson@binus.ac.id>
2024-11-26 15:01:26 +0700

    2nd

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Use SQLite for simplicity
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.ykrmemoqdxuwljqnbbcy:IW4Jq2jHJruL9g5Z@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"  # Use SQLite for simplicity
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
